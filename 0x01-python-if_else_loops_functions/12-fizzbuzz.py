e5583eb52bafc261b43b7176bf72a6e923f8c06a
adepegba1 <adepegbadavid@rocketmail.com>
2022-12-07 01:26:25 -0800

    task 12' git push

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+for i in range(1, 101):
+    if (i % 3 == 0):
+        print("Fizz", end=" ")
+    elif (i % 5 == 0):
+        if i == 100:
+            print("Buzz")
+        else:
+            print("Buzz", end=" ")
+    elif (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz", end=" ")
+    else:
+        print(i, end=" ")
