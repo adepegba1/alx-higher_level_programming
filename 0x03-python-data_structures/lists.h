3417d9e7af47bbd40d2fdc4df1bd132ef05959ff
adepegba1 <adepegbadavid@rocketmail.com>
2022-12-27 01:24:07 -0800

    task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -10,7 +10,6 @@
   * Description: singly linked list node structure
   * for project
   */
-int is_palindrome(listint_t **head);
 typedef struct listint_s
 {
 	int n;
@@ -20,4 +19,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 #endif
