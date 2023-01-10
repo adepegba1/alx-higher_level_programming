5bd023c6962afcf289142e817655f6903c0f21b6
adepegba1 <adepegbadavid@rocketmail.com>
2023-01-10 03:36:41 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
+
+
 class Square:
     """Represent a square."""
     def __init__(self, size=0):
@@ -12,6 +14,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
