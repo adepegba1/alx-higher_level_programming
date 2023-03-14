f7b5310f9dad1a842a8e865ce91262ef56f1061e
adepegba1 <adepegbadavid@rocketmail.com>
2023-03-14 12:14:54 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const  argCount= process.argv.length;
 if (argCount > 2)
-	console.log('Argument found');
+	console.log('Argument' + (argCount > 3 ? 's ' : ' ') + 'found');
 else
 	console.log('No argument');
