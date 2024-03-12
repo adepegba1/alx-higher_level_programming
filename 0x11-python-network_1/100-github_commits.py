faeba21ccb53931d91a25b5c9356e7c56479a32d
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2024-03-12 15:15:55 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 import sys
 import requests
