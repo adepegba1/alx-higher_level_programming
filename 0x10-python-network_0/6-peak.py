ffd5b2fdc3ad87e3ec1a0f2a3b4a3119d205d2b1
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2024-03-12 15:10:32 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""task 6 modele"""
+"""
+Module to find the hightest number in an array
+"""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """
+    Method to find hightest number in array
+    """
+    if bool(list_of_integers) is False:
         return None
-
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    list_of_integers.sort()
+    return list_of_integers[-1]
