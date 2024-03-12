408f18e1cddc710d961f514b6c7c9fba698f923f
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2024-03-12 15:14:35 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
+"""fetches the url https://intranet.hbtn.io/status"""
+import urllib.request
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf = content.decode('utf-8')
+        print('Body response:')
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(utf))
