535cdc7c81e528b01a2a586bbc6e0d7177910790
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2024-03-12 15:20:38 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
 """
-import sys
+Takes in a URL, sends a request to the URL and displays the body
+of the response
+"""
+from sys import argv
 import requests
 
-
 if __name__ == "__main__":
-    url = sys.argv[1]
-
+    url = argv[1]
     r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
+    try:
+        r.raise_for_status()
         print(r.text)
+    except Exception as e:
+        print("Error code: {}".format(r.status_code))
